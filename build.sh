bcdea1a951c05d0b9db21645fe213de553a703e8
Enrico Campidoglio <enrico.campidoglio@btj.se>
2019-02-18 11:05:01 +0100

    Modifies the bootstrapper to build without Mono on macOS and Linux

--- build.sh
+++ build.sh
@@ -1,88 +1,27 @@
 #!/usr/bin/env bash
+########################################################################################
+# This is a Cake bootstrapper script for Linux and macOS using .NET Core 2.0
+# Inspired by
+# https://adamhathcock.blog/2017/07/12/net-core-on-circle-ci-2-0-using-docker-and-cake/
+########################################################################################
 
-##########################################################################
-# This is the Cake bootstrapper script for Linux and OS X.
-# This file was downloaded from https://github.com/cake-build/resources
-# and modified to run on .NET Core instead of Mono
-##########################################################################
-
-# Define directories.
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+CAKE_VERSION=0.30.0
 TOOLS_DIR=$SCRIPT_DIR/tools
-NUGET_DIR=$TOOLS_DIR/NuGet
-NUGET_EXE=$NUGET_DIR/nuget.exe
-NUGET_SHIM=$TOOLS_DIR/nuget.exe.sh
-NUGET=$TOOLS_DIR/nuget.exe
-CAKE_VERSION="0.30.0"
-CAKE_DLL=$TOOLS_DIR/Cake.CoreCLR/Cake.dll
+TOOLS_PROJ=$TOOLS_DIR/tools.csproj
+CAKE_DLL=$TOOLS_DIR/Cake.CoreCLR.$CAKE_VERSION/cake.coreclr/$CAKE_VERSION/Cake.dll
 
-# Define default arguments.
-SCRIPT="build.cake"
-TARGET="Default"
-CONFIGURATION="Release"
-VERBOSITY="verbose"
-DRYRUN=
-SHOW_VERSION=false
-SCRIPT_ARGUMENTS=()
-
-# Parse arguments.
-for i in "$@"; do
-    case $1 in
-        -s|--script) SCRIPT="$2"; shift ;;
-        -t|--target) TARGET="$2"; shift ;;
-        -c|--configuration) CONFIGURATION="$2"; shift ;;
-        -v|--verbosity) VERBOSITY="$2"; shift ;;
-        -d|--dryrun) DRYRUN="-dryrun" ;;
-        --version) SHOW_VERSION=true ;;
-        --) shift; SCRIPT_ARGUMENTS+=("$@"); break ;;
-        *) SCRIPT_ARGUMENTS+=("$1") ;;
-    esac
-    shift
-done
-
-# Make sure the tools folder exist.
+# Make sure the tools folder exist
 if [ ! -d "$TOOLS_DIR" ]; then
   mkdir "$TOOLS_DIR"
 fi
 
-# Make sure the NuGet folder exist.
-if [ ! -d "$NUGET_DIR" ]; then
-    mkdir "$NUGET_DIR"
-fi
-
-# Download NuGet if it does not exist.
-if [ ! -f "$NUGET_EXE" ]; then
-    echo "Downloading NuGet..."
-    curl -Lsfo "$NUGET_EXE" https://dist.nuget.org/win-x86-commandline/v3.4.4/nuget.exe
-    if [ $? -ne 0 ]; then
-        echo "An error occured while downloading nuget.exe."
-        exit 1
-    fi
-fi
-
-# Rename a copy of the NuGet shim to the NuGet executable.
-if [ ! -f "$NUGET" ]; then
-    cp "$NUGET_SHIM" "$NUGET"
-fi
-
-# Install Cake.CoreCRL from NuGet.
+# Install Cake.CoreCLR
 if [ ! -f "$CAKE_DLL" ]; then
-    "$NUGET" install Cake.CoreCLR -Version $CAKE_VERSION -ExcludeVersion -OutputDirectory "$TOOLS_DIR"
-    if [ $? -ne 0 ]; then
-        echo "Could not install the Cake.CoreCLR package from NuGet"
-        exit 1
-    fi
+    echo -e "\e[92mInstalling Cake $CAKE_VERSION for .NET Core in '$TOOLS_DIR'\e[0m"
+    echo "<Project Sdk=\"Microsoft.NET.Sdk\"><PropertyGroup><OutputType>Exe</OutputType><TargetFramework>netcoreapp2.0</TargetFramework></PropertyGroup></Project>" > $TOOLS_PROJ
+    dotnet add $TOOLS_PROJ package Cake.CoreCLR -v $CAKE_VERSION --package-directory $TOOLS_DIR/Cake.CoreCLR.$CAKE_VERSION
 fi
 
-# Make sure that Cake has been installed.
-if [ ! -f "$CAKE_DLL" ]; then
-    echo "Could not find Cake at '$CAKE_DLL'."
-    exit 1
-fi
-
-# Start Cake
-if $SHOW_VERSION; then
-    dotnet "$CAKE_DLL" -version
-else
-    dotnet "$CAKE_DLL" $SCRIPT -verbosity=$VERBOSITY -configuration=$CONFIGURATION -target=$TARGET $DRYRUN "${SCRIPT_ARGUMENTS[@]}"
-fi
+# Run the build script
+exec dotnet "$CAKE_DLL" "$@"
--- tools/nuget.exe.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-NUGET_EXE=$SCRIPT_DIR/NuGet/nuget.exe
-ARGUMENTS="$@"
-
-mono $NUGET_EXE $ARGUMENTS
